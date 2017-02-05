67237b73985a0ebb7d78505cd727df299ed6d9c0
RehamElhadi <RehamElhadi@users.noreply.github.com>
2017-02-05 22:09:00 +0300

    order insert

--- std.c
+++ std.c
@@ -12,31 +12,23 @@ main(){
 	FILE *fp ;
 	FILE *temp ;
 	int id = 1 ;
-/*	FILE *Id ; 
-	Id = fopen("FId","r");
-	fscanf(Id, "%d",&id);
-	fclose(Id);*/
+	int fid ;
 	char c ;
-
-	//file add ;
-	//*fp = fopen("myfile.txt" ,"w");
 	while(loophandler != 0){
 		printf("   Menu  \n");
 		printf("1.create new file .\n2.insert new student .\n3.view all student .\n4.search.\n5.delete student .\n0.exit .");
 		scanf("%d",&casevar);
-		//loophandler = 0 ;
-		switch(casevar){
+				switch(casevar){
 			case 1 :printf("file name :");
 					char fname[10] ;
 					scanf("%s",fname);
 					fp = fopen(fname ,"w");
 					fclose(fp);
 					break ;
-			case 2 ://student.id = id ; 
+			case 2 :
 					id++;
 					fp = fopen("myfile.txt" ,"r");
-					temp = fopen("temp","w");
-					//fseek(fp,0,SEEK_END);
+					temp = fopen("temp.txt","w+");
 					printf("student id :");
 					scanf("%d",&student.id);
 					printf("student name :");
@@ -44,17 +36,26 @@ main(){
 					printf("student degree in database :");
 					scanf("%s",student.degree);
 					student.deleted = 0 ;
-					while(!feof(fp)){	
-					fscanf(fp,"%d %d %s %s\n",&std.id,&std.deleted,std.name,std.degree);
-					if(std.id < student.id)
-					fprintf( temp,"%d %d %s %s\n",&std.id,&std.deleted,std.name,std.degree);
-					else
-					fprintf( temp,"%d %d %s %s\n",student.id,student.deleted,student.name,student.degree );	
+					int x=1;
+					
+					int iswrite=0;	
+					while(!feof(fp)){
+					int r=fscanf(fp,"%d %d %s %s\n",&fid,&std.deleted,std.name,std.degree);
+					if(r>0 &&(fid < student.id || iswrite > 0))
+					fprintf( temp,"%d %d %s %s\n",fid,std.deleted,std.name,std.degree);
+					else if(r>0){
+					fprintf( temp,"%d %d %s %s\n",student.id,student.deleted,student.name,student.degree );
+					fprintf( temp,"%d %d %s %s\n",fid,std.deleted,std.name,std.degree);
+					++iswrite;
+					}	
+					++x;
 					}
+					if(iswrite == 0)
+						fprintf( temp,"%d %d %s %s\n",student.id,student.deleted,student.name,student.degree );
 					fclose(temp);
 					fclose(fp);
 					temp = fopen("temp.txt","r");
-					fp = fopen("myfile.txt" ,"w" );
+					fp = fopen("myfile.txt" ,"w+" );
 
 					while(!feof(temp)){
 											fscanf(temp,"%d %d %s %s\n",&student.id,&student.deleted,student.name,student.degree);
@@ -68,12 +69,9 @@ main(){
 					fp = fopen("myfile.txt" ,"r" );
 					printf("kkkk\n");
 					int p = id-1 ;
-					while(p > 0){
-						printf("%d\n",p);
+					while(!feof(fp)){
 						fscanf(fp,"%d %d %s %s\n",&student.id,&student.deleted,student.name,student.degree);
-						//if(student.deleted != 1)
-						printf("%d&%s&%s\n",student.id,student.name,student.degree);
-						p--;
+						printf("%d %s %s\n",student.id,student.name,student.degree);
 					}
 					fclose(fp);
 					break ;
@@ -84,7 +82,7 @@ main(){
 					int key ;
 					scanf("%d",&key);
 					int p2 = 0 ;
-					while(p2 < id){
+					while(!feof(fp)){
 					//printf("%d\n",p);
 					fscanf(fp,"%d %d %s %s\n",&student.id,&student.deleted,student.name,student.degree);
 						if(student.id == key){
@@ -101,7 +99,7 @@ main(){
 					int key2 ;
 					scanf("%d",&key2);
 					int p3 = 0 ;
-					while(p3 < id){
+					while(!feof(fp)){
 					//printf("%d\n",p);
 					fscanf(fp,"%d %d %s %s\n",&student.id,&student.deleted,student.name,student.degree);
 						if(student.id != key2){
